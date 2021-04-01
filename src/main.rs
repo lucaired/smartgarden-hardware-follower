ce0bb71d04f207622f23a645470e8f64bb9279c4
lucaired <aired@posteo.hu>
2021-04-01 14:48:17 +0200

    add rocket/reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+extern crate reqwest;
+#[macro_use] extern crate rocket;
+
+use rocket::http::RawStr;
+
+use std::time::Duration;
+use reqwest::ClientBuilder;
+
+
+#[get("/fan/on")]
+fn fan_on() -> Result<String, Box<std::error::Error>> {
+    let request_url = format!("https://api.github.com/users/{}", user);
+    
+    let timeout = Duration::new(5, 0);
+    let client = ClientBuilder::new().timeout(timeout).build()?;
+    let response = client.head(&request_url).send()?;
+
+    if response.status().is_success() {
+        Ok(format!("{} is a user!", user))
+    } else {
+        Ok(format!("{} is not a user!", user))
+    }
+}
+
+#[get("/fan/on")]
+fn fan_off() -> Result<String, Box<std::error::Error>> {
+    // TODO: call to usb control
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index, check]).launch();
 }
